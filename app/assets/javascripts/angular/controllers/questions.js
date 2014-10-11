1fb9da08463da02ebca24a93ac52d4ea31b36678
Oleg Pasko <olegpasko@gmail.com>
2014-10-11 20:20:05 +0300

    checkCurrentUser for new answers

--- app/assets/javascripts/angular/controllers/questions.js
+++ app/assets/javascripts/angular/controllers/questions.js
@@ -15,7 +15,7 @@ app.controller('QuestionsCtrl', ['$scope', '$http', function($scope, $http) {
     });
   };
   $scope.fetchQuestions();
-}]).controller('QuestionDetailsCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+}]).controller('QuestionDetailsCtrl', ['$scope', '$http', '$state', 'toaster', function($scope, $http, $state, toaster) {
   $http.get('/questions/'+ $state.params.id +'.json').success(function(data) {
     $scope.question = data.question;
     $scope.question.created_at_from_now = moment($scope.question.created_at).fromNow();
@@ -41,9 +41,14 @@ app.controller('QuestionsCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.credentials = {
     body: '',
     authenticity_token: $scope.authenticity_token
-  }
+  };
+  $scope.checkCurrentUser = function() {
+    if (!$scope.currentUser) {
+      toaster.pop('info', '', 'Треба спочатку увійти до системи :)');
+    }
+  };
 }]).controller('NewQuestionCtrl', ['$scope', '$http', '$state', 'toaster', function($scope, $http, $state, toaster) {
-  // validate current user
+  // validate current user, TODO: refactor this
   if (!$scope.currentUser) {
     $state.go('app.questions');
     toaster.pop('info', '', 'Треба спочатку увійти до системи :)');
