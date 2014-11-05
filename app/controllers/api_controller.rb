58cd7f6bf8fd325fd584f8dbb93072abd9ac57c2
Oleg Pasko <olegpasko@gmail.com>
2014-11-05 17:06:33 +0200

    Access-Control-Allow-Origin

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,15 @@
 # coding: utf-8
 class ApiController < ApplicationController
 
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
   def parse_video
     pkg = params[:pkg]
 
