3622b7124df0e40d75ce180d580531965ad5e041
Oleg Pasko <olegpasko@gmail.com>
2015-01-10 16:14:06 +0200

    scope changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User
       user.provider = auth['provider']
       user.uid = auth['uid']
       if auth['info']
-        user.name = auth['info']['name'] || ''
+        user.name = auth['info']['name'] || auth['info']['email'].split('@')[0] || 'anonymous'
         user.email = auth['info']['email'] || ''
       end
     end
